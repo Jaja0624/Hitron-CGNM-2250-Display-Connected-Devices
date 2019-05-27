45f5dcab61a877479c17546f8d1d2626ad49cc4d
jsa275 <jsa275@sfu.ca>
2019-05-27 05:49:58 -0700

    added readme and comments

--- devices.py
+++ devices.py
@@ -4,14 +4,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
+# Your router login credentials
 myUsername = ""
 myPassword = ""
-
-driver = webdriver.Chrome()
-
 loginUrl = 'http://192.168.0.1/login.htm'
 
+# initialize selenium webdriver 
+# opens chrome browser to "loginUrl"
+driver = webdriver.Chrome()
+
 driver.get(loginUrl)
+
 wait = WebDriverWait(driver, 15)
 
 username = wait.until(EC.visibility_of_element_located((By.NAME, "user[login]")))
@@ -29,16 +32,20 @@ loginBtn.click()
 
 wait.until(EC.visibility_of_element_located((By.ID, "clientsDiv")))
 
+# url with show btn for connected devices
 devicesUrl = 'http://192.168.0.1/index.htm#basic_lan/m/2/s/0'
 
 driver.get(devicesUrl)
 
+# find and press the show btn
 showBtn = wait.until(EC.visibility_of_element_located((By.ID, "btnShowConnectedDevice")))
 showBtn.click()
 
 devicesTable = wait.until(EC.visibility_of_element_located((By.ID, "tblConnectDevice")))
-time.sleep(5) # necessary to wait for the table to load completely
-
+time.sleep(4) # necessary to wait for the table to load completely (i think)
 for row in devicesTable.find_elements_by_xpath('./tbody/tr'):
     col = row.find_elements(By.TAG_NAME, "td")
     print(col[0].text)
+
+# close all browsers
+driver.quit()
---
+++ readme.md
@@ -0,0 +1,21 @@
+# Display connected devices for Hitron CGNM-2250 Router (Shaw)
+
+Uses Selenium Webdriver to login to your router and display connected devices. 
+Easily modified for other purposes.
+
+Yes, there is likely a much better method than having to open a web browser.
+
+## Requirements
+
+Selenium + Chromedriver
+```
+pip install selenium
+```
+
+```
+pip install chromedriver
+```
+
+### Working as of software version: 4.5.11.9.1
+
+### P.S. The modem has a limit on the number of times you can login... "30 per session"
